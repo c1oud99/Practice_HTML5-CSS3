5ad3c305b0288122c430d0ca53e292b9f647755f
c1oud99 <1imepie.c1oud9@gmail.com>
2025-03-01 15:28:08 +0900

    add

--- project/Calculator/index.js
+++ project/Calculator/index.js
@@ -18,3 +18,33 @@ document.querySelector(".equal").addEventListener("click", () => {
         display.innerText = "Error";
     }
 });
+
+
+function clearDisplay() {
+    display.innerText = "";
+}
+
+document.querySelector(".clear").addEventListener("click", clearDisplay);
+
+
+document.querySelector(".clear").addEventListener("click", () => {
+    display.innerText = "";
+});
+
+document.addEventListener("keydown", (event) => {
+    const key = event.key;
+    
+    if (!isNaN(key) || ["+", "-", "*", "/"].includes(key)) {
+        display.innerText += key;
+    } else if (key === "Enter") {
+        try {
+            display.innerText = eval(display.innerText);
+        } catch {
+            display.innerText = "Error";
+        }
+    } else if (key === "Backspace") {
+        display.innerText = display.innerText.slice(0, -1);
+    } else if (key === "Escape") {
+        display.innerText = "";
+    }
+});
